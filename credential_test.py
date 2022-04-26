4a1a31af59e75af77a4f62bee00ecac455cff919
janeffer945 <janeffernjeri945@gmail.com>
2022-04-26 16:58:04 +0300

    add import credentials

---
+++ credential_test.py
@@ -0,0 +1,9 @@
+import unittest
+from credentials import Credentials
+
+class TestCredentials(unittest.TestCase) :
+    '''
+    Test class that defines test cases for the Credentials class behaviours.
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
