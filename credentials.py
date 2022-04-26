6f289c2b4f60c2408df68c9a182244f9cef131ca
janeffer945 <janeffernjeri945@gmail.com>
2022-04-26 17:11:55 +0300

    add def

--- credentials.py
+++ credentials.py
@@ -5,3 +5,11 @@ class Credentials:
 
     accounts_list = [] 
     
+    def __init__(self, acc_name, user_name, user_password):
+        '''
+        method that defines variables to be used
+        '''
+
+        self.acc_name = acc_name
+        self.user_name = user_name
+        self.user_password = user_password
