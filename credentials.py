dc7fe29d1d1a101f0048dab65dc091f93d8e1d35
janeffer945 <janeffernjeri945@gmail.com>
2022-04-26 17:13:03 +0300

    add class methond

--- credentials.py
+++ credentials.py
@@ -13,3 +13,22 @@ class Credentials:
         self.acc_name = acc_name
         self.user_name = user_name
         self.user_password = user_password
+@classmethod
+    def display_accounts(cls):
+        '''
+        method that returns the accounts_list
+        '''
+        return cls.accounts_list
+    
+        
+    @classmethod
+    def save_credential(self):
+
+        '''
+        save_credential method saves account objects into accounts_list
+        '''
+
+        Credentials.accounts_list.append(self)
+    @classmethod
+    def find_credential(cls,acc_name):
+       
