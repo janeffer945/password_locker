ffc58189582335f5fd24cdd25a97f580be689ac6
janeffer945 <janeffernjeri945@gmail.com>
2022-04-26 14:40:32 +0300

    add unittest

---
+++ user_test.py
@@ -0,0 +1,15 @@
+import unittest #importing the unittest module
+from user import User  #importing the user class
+
+class TestUser(unittest.TestCase):
+
+
+      '''
+      Test class that defines test cases for the contact class behaviours.
+      Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+      '''
+      def setUp(self):
+          """
+          Set up method to run before each test cases.
+          """
