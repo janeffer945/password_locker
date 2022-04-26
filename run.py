4770307524779c6e7d0923497c63de6e21c0d4bb
janeffer945 <janeffernjeri945@gmail.com>
2022-04-26 17:20:13 +0300

    add def save

--- run.py
+++ run.py
@@ -1,3 +1,19 @@
 #!/usr/bin/env python3.8
 from credentials import Credentials
 from user import User
+
+def create_account(acc_name, user_name, user_password):
+    new_account= Credentials(acc_name, user_name, user_password)
+    return new_account
+
+
+def save_credential (credentials):
+    '''
+    Function to delete a credentials
+    '''
+    credentials.save_credential()
+def find_credential(acc_name):
+    """
+    find a credential from the credential list using account
+    """
+    return Credentials.find_credential(acc_name)
