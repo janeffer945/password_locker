029139b0d43fec5800874c6d63808b21dec384a0
janeffer945 <janeffernjeri945@gmail.com>
2022-04-26 17:03:00 +0300

    add test

--- credential_test.py
+++ credential_test.py
@@ -7,3 +7,21 @@ class TestCredentials(unittest.TestCase) :
     Args:
         unittest.TestCase: TestCase class that helps in creating test cases
     '''
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_account = Credentials("Facebook","janeffer", "0746397945") 
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_account.acc_name, "Facebook")
+        self.assertEqual(self.new_account.user_name, "janeffer")
+        self.assertEqual(self.new_account.user_password,"0746397994")
+        
+        
+        
+       
+       
