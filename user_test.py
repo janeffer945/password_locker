9719ea9c45314c50854e6dc16481cae12e6dc1eb
janeffer945 <janeffernjeri945@gmail.com>
2022-04-26 14:52:03 +0300

    add users

--- user_test.py
+++ user_test.py
@@ -13,3 +13,41 @@ class TestUser(unittest.TestCase):
           """
           Set up method to run before each test cases.
           """
+           self.new_user = User("janefer", "janeffer945")# create user object
+
+      def test__init(self):
+          """
+          test__init test case to test if the object is initialized properly
+          """
+          self.assertEqual(self.new__user.user_name, "janeffer")
+          self.assertEqual(self.new_user.user_name, "janeffer")
+
+      def test_save_user(self):
+          '''
+          test_save_contact test case to test if the user object is saved into
+          the user list
+          '''
+          self.new_user.save_user() # saving the new user
+          self.assertEqual(len(User.user_list),1)
+ def test_save_multiple_user(self):
+        '''
+        test_save_multiple_user to check if we can save multiple user
+        objects to our user_list
+        '''
+        self.new_user.save_user()
+        test_user = User("abby", "abbis")
+        # new user
+        test_user.save_user()
+        self.assertEqual(len(User.user_list),2)
+ def tearDown(self):
+        '''
+        tearDown method that does clean up after each test case has run.
+        '''
+        User.user_list = []
+ def test_save_multiple_user(self):
+        '''
+        test_save_multiple_user to check if we can save multiple user
+        objects to our user_list
+        '''
+        self.new_user.save_user()
+        test_user = User()
