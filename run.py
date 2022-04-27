b93cb065ab741bcd795480beb34b0f84e839deda
janeffer945 <janeffernjeri945@gmail.com>
2022-04-27 10:29:48 +0300

    final commit

--- run.py
+++ run.py
@@ -1,24 +1,29 @@
-#!/usr/bin/env python3.8
 from credentials import Credentials
 from user import User
 
+
 def create_account(acc_name, user_name, user_password):
-    new_account= Credentials(acc_name, user_name, user_password)
+    new_account = Credentials(acc_name, user_name, user_password)
     return new_account
 
 
-def save_credential (credentials):
+def save_credential(credentials):
     '''
     Function to delete a credentials
     '''
     credentials.save_credential()
+
+
 def find_credential(acc_name):
     """
     find a credential from the credential list using account
     """
     return Credentials.find_credential(acc_name)
+
+
 def display_accounts():
     '''
+    Function that returns all the saved accounts
     Function that returns all the saved accounts
     '''
     return Credentials.display_accounts()
@@ -29,6 +34,8 @@ def del_account(Credentials):
     Function to delete an account
     '''
     Credentials.delete_account()
+
+
 def main():
     print("Hello Welcome to your password locker site. What is your name?")
     name = input()
@@ -36,14 +43,13 @@ def main():
     print(f"Hello {name}. what would you like to do?")
     print('\n')
 
-
     while True:
-        print("Hello. Welcome back to Password Locker!")
+        print("Hello. Welcome back to Password Lock")
         print('\n')
         print('Create a login username and a password')
         print('\n')
         print('Enter username')
-        username =  input()
+        username = input()
         print('Enter password')
         user_password = input()
 
@@ -52,3 +58,66 @@ def main():
         if (input_username == username):
             print('Enter your password')
             input_password = input()
+            if (input_password == user_password):
+                print("Use these short codes : cc - create a new credential \n, ̣di - display accounts \n, fi -find an account \n, ex -exit the account list \n")
+                
+
+                short_code = input("").lower().strip()
+
+                if short_code == "cr":
+                    print("New Account")
+                    print("-"*10)
+
+                    print("Account name ...")
+                    acc_name = input()
+                    print("Username ...")
+                    user_name = input()
+
+                    print("Password ...")
+                    user_password = input()
+
+                    # create and save new account.
+                    save_credential(create_account(
+                        acc_name, user_name, user_password))
+                    print('\n')
+                    print(f"New {acc_name} Account Created")
+                    print("\n\n")
+                    # print("Here are your account details")
+                    # print(f"ACCOUNT_NAME: {acc_name}\nUSERNAME: {user_name}\nUSER_PASSWORD: {user_password}")
+
+                    print('\n')
+                elif short_code == "di":
+                    if display_accounts:
+                        print("Below is a list of all your accounts")
+                        print('\n')
+
+                        for account in display_accounts():
+                            print(
+                                f"{account.acc_name}{account.user_name} .....{account.user_password}")
+
+                        print('\n')
+
+                    else:
+                        print(' account not found,kindly create a new account')
+            elif short_code == "fi":
+                    print("Enter the Account Name you want to search for")
+                    search_name = input().lower()
+                    if find_credential(search_name):
+                        search_credential = find_credential(search_name)
+                        print(f"Account Name : {search_credential.acc_name}")
+                        print('-' * 60)
+                        print(
+                            f"User Name: {search_credential.name} Password :{search_credential.password}")
+                        print('-' * 60)
+                    else:
+                        print("The Credential does not exist")
+                        print('\n')
+
+            else:
+                print('choose short code to continue')
+
+        else: 
+            print('Wrong Password')
+if __name__ == '__main__':
+    main()     
+               
--- user_test.py
+++ user_test.py
@@ -1,104 +1,57 @@
-# import unittest #importing the unittest module
-# from user import User  #importing the user class
+import unittest
+from credentials import Credentials
 
-# class TestUser(unittest.TestCase):
+class TestCredentials(unittest.TestCase) :
+    '''
+    Test class that defines test cases for the Credentials class behaviours.
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_account = Credentials("Facebook","janeffer", "0746397945") 
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_account.acc_name, "Facebook")
+        self.assertEqual(self.new_account.user_name, "janeffer")
+        self.assertEqual(self.new_account.user_password,"0746397994")
+    def test_display_all_accounts(self):
+        '''
+        method that returns a list of saved accounts
+        '''
+
+        self.assertEqual(Credentials.display_accounts(), Credentials.accounts_list)
 
 
-#       '''
-#       Test class that defines test cases for the contact class behaviours.
-#       Args:
-#         unittest.TestCase: TestCase class that helps in creating test cases
-#       '''
-#       def setUp(self):
-#           """
-#           Set up method to run before each test cases.
-#           """
-#           self.new_user = User("janefer", "janeffer945")# create user object
+    def test_save_all_accounts(self):
+        '''
+        method that saves a new account
+        '''
+        self.new_account.save_credential()  
+        test_account = Credentials("Facebook","janeffer","0746397994") #new account
+        test_account.save_credential()
+        self.assertEqual(len(Credentials.accounts_list), 3)
 
-#       def test__init(self):
-#           """
-#           test__init test case to test if the object is initialized properly
-#           """
-#           self.assertEqual(self.new__user.user_name, "janeffer")
-#           self.assertEqual(self.new_user.user_name, "janeffer")
 
-#       def test_save_user(self):
-#           '''
-#           test_save_contact test case to test if the user object is saved into
-#           the user list
-#           '''
-#           self.new_user.save_user() # saving the new user
-#           self.assertEqual(len(User.user_list),1)
-#       def test_save_multiple_user(self):
-#         '''
-#         test_save_multiple_user to check if we can save multiple user
-#         objects to our user_list
-#         '''
-#         self.new_user.save_user()
-#         test_user = User("abby", "abbis")
-#         # new user
-#         test_user.save_user()
-#         self.assertEqual(len(User.user_list),2)
-#       def tearDown(self):
-#         '''
-#         tearDown method that does clean up after each test case has run.
-#         '''
-#         User.user_list = []
-#       def test_save_multiple_user(self):
-#         '''
-#         test_save_multiple_user to check if we can save multiple user
-#         objects to our user_list
-#         '''
-#         self.new_user.save_user()
-#         test_user = User("abby", "abbis")
-#         test_user.save_user()
-#         self.assertEqual(len(User.user_list),2) 
-#       def test_delete_user(self):
-#         '''
-#         test_delete_user to test if we can remove a contact from our user list
-#         '''
-#         self.new_user.save_user()
-#         test_user = User("abby", "abbis")
-#  # new user
-#         test_user.save_user()
+    def test_delete_account(self):
+        '''
+        a test to know if we can delete an account.
+        '''
+        self.new_account.save_credential()
+        test_account = Credentials("Facebook","janeffer", "0746397994")
+        test_account.save_credential()
 
-#         self.new_user.delete_user()# Deleting a user object
-#         self.assertEqual(len(User.user_list),1)
+        self.new_account.delete_account()
+        self.assertEqual(len(Credentials.accounts_list), 1)
 
-#       def test_find_user_by_user_name(self):
-#         '''
-#         test to check if we can find a user by username and display information
-#         '''
-#         self.new_user.save_user()
-#         test_user = User("abby", "abbis") # new user
-#         test_user.save_user()
 
-#         found_user = User.find_by_user_name("abby")
-#         self.assertEqual(found_user.user_name,test_user.user_name)   
+if __name__ == '__main__':
+    unittest.main()
+
        
-
-#       def test_user_exists(self):
-#         '''
-#         test to check if we can return a Boolean  if we cannot find the user.
-#         '''
-
-#         self.new_user.save_user()
-#         test_user = User("abby", "abbis")#new user
-#         test_user.save_user()
-
-#         user_exists = User.user_exist("abby")
-#         self.assertTrue(user_exists)
-
-
-#       def test_display_all_users(self):
-#         '''
-#         method that returns a list of users saved
-#         '''
-
-#         self.assertEqual(User.display_users(),User.user_list)
-
-
-
-
-        
-        
+       
